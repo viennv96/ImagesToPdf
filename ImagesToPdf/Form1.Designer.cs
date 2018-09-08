61851b64dad7eaef1f3242b336d95931f140e1c2
Nguyễn Văn Viên <30931880+viennv96@users.noreply.github.com>
2018-09-09 01:59:19 +0700

    Initial commit

---
+++ ImagesToPdf/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace ImagesToPdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.ssStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowDrop = true;
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(611, 295);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragEnter);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Input";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "Output Name";
+            this.Column2.Name = "Column2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(506, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 31);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save To ...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ssStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 362);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(611, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // ssStatus
+            // 
+            this.ssStatus.ActiveLinkColor = System.Drawing.Color.Red;
+            this.ssStatus.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ssStatus.ForeColor = System.Drawing.Color.Blue;
+            this.ssStatus.Name = "ssStatus";
+            this.ssStatus.Size = new System.Drawing.Size(42, 17);
+            this.ssStatus.Text = "Status";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(611, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addToolStripMenuItem
+            // 
+            this.addToolStripMenuItem.Name = "addToolStripMenuItem";
+            this.addToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.addToolStripMenuItem.Text = "Add";
+            this.addToolStripMenuItem.Click += new System.EventHandler(this.addToolStripMenuItem_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "0°",
+            "90°",
+            "180°",
+            "270°"});
+            this.comboBox1.Location = new System.Drawing.Point(57, 334);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(67, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Rotate";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 384);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Images to PDFs by VienNV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel ssStatus;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem addToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ImagesToPdf/Form1.cs
@@ -0,0 +1,212 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ImagesToPdf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileFolderDialog sfd = new FileFolderDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                Thread t = new Thread(() =>
+                {
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+                        row.DefaultCellStyle.BackColor = Color.Yellow;
+                        ssStatus.Text = String.Format("Đang xử lý {0}!", row.Cells[1].Value.ToString());
+                        try
+                        {
+                            List<string> images = listAllImages(row.Cells[0].Value.ToString());
+                            ImagesFoldersToPdf(images, sfd.SelectedPath, row.Cells[1].Value.ToString());
+                            ssStatus.Text = String.Format("Xử lý {0} hoàn tất!", row.Cells[1].Value.ToString());
+                            row.DefaultCellStyle.BackColor = Color.LimeGreen;
+                        }
+                        catch (Exception ex)
+                        {
+                            ssStatus.Text = String.Format("Xử lý {0} Lỗi!", row.Cells[1].Value.ToString());
+                            row.DefaultCellStyle.BackColor = Color.Red;
+                            MessageBox.Show(ex.ToString(), "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                    }
+                    MessageBox.Show(String.Format("Completed!\nAll File Save here \"{0}\"", sfd.SelectedPath), "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                });
+                t.Start();
+            }
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FileFolderDialog ffd = new FileFolderDialog();
+            if (ffd.ShowDialog() == DialogResult.OK)
+            {
+                AddDataToTable(ffd.SelectedPath);
+            }
+        }
+
+        private void dataGridView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] items = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string item in items)
+            {
+                AddDataToTable(item);
+            }
+        }
+
+        void AddDataToTable(string folderPath)
+        {
+            string[] folders = Directory.GetDirectories(folderPath);
+            if (folders.Length != 0)
+            {
+                foreach (string folder in folders)
+                {
+                    string outputName = new DirectoryInfo(folder).Name;
+                    dataGridView1.Rows.Add(folder, string.Format("{0}.pdf", outputName));
+                }
+            }
+            else
+            {
+                string outputName = new DirectoryInfo(folderPath).Name;
+                dataGridView1.Rows.Add(folderPath, string.Format("{0}.pdf", outputName));
+            }
+        }
+
+        List<string> listAllImages(string folderPath)
+        {
+            List<string> images = new List<string>();
+            string[] all = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+            foreach (string record in all)
+            {
+                if (Directory.Exists(record))
+                {
+                    listAllImages(record);
+                }
+                else
+                {
+                    FileInfo fi = new FileInfo(record);
+                    if (fi.Extension.ToLower() == ".png" || fi.Extension.ToLower() == ".jpg" || fi.Extension.ToLower() == ".jpeg" || fi.Extension.ToLower() == ".bmp")
+                    {
+                        images.Add(record);
+                    }
+                }
+            }
+            return images;
+        }
+
+        void ImagesFoldersToPdf(List<string> images, string outputFolderPath, string outputFileName)
+        {
+            PdfDocument ouput = new PdfDocument();
+
+            foreach (string image in images)
+            {
+
+                XImage im = XImage.FromFile(image);
+
+                if (File.Exists("temp.png"))
+                {
+                    File.Delete("temp.png");
+                }
+
+                var bitmap = Bitmap.FromFile(image);
+
+                switch (comboBox1.SelectedIndex)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        bitmap.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        break;
+                    case 2:
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        break;
+                    case 3:
+                        bitmap.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        break;
+                }
+
+
+                if (im.Width > im.Height)
+                {
+                    bitmap.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                }
+                bitmap.Save("temp.png", ImageFormat.Png);
+                im.Dispose();
+                bitmap.Dispose();
+
+                XImage img = XImage.FromFile("temp.png");
+
+                // each source file saeparate
+                PdfDocument doc = new PdfDocument();
+                PdfPage p = new PdfPage();
+
+                double width = img.Width;
+                double height = img.Height;
+                double w = 0, h = 0;
+                double rate = width / height;
+                double x = 0, y = 0;
+
+                if (rate < (p.Width / p.Height))
+                {
+                    //p.Orientation = PdfSharp.PageOrientation.Portrait;
+                    h = p.Height;
+                    w = rate * h;
+                    x = (p.Width - w) / 2;
+                }
+                else
+                {
+                    //p.Orientation = PdfSharp.PageOrientation.Landscape;
+                    w = p.Width;
+                    h = w / rate;
+                    y = (p.Height - h) / 2;
+                }
+
+                doc.Pages.Add(p);
+                XGraphics xgr = XGraphics.FromPdfPage(doc.Pages[0]);
+                xgr.DrawImage(img, x, y, w, h);
+                img.Dispose();
+                xgr.Dispose();
+                //  save to destination file
+                FileInfo fi = new FileInfo("temp.png");
+                doc.Save(fi.FullName.Replace(fi.Extension, ".PDF"));
+
+                PdfDocument inputDocument = PdfReader.Open(fi.FullName.Replace(fi.Extension, ".PDF"), PdfDocumentOpenMode.Import);
+                PdfPage page = inputDocument.Pages[0];
+                ouput.AddPage(page);
+                File.Delete(fi.FullName.Replace(fi.Extension, ".PDF"));
+                doc.Close();
+                doc.Dispose();
+            }
+            ouput.Save(string.Format("{0}\\{1}", outputFolderPath, outputFileName));
+            ouput.Close();
+            ouput.Dispose();
+        }
+    }
+}
